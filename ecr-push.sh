d0494f8fd4ba52da849029fadc025981c271477b
agauli <apps.ashu@gmail.com>
2024-08-14 01:19:47 -0400

    Update ecr-push.sh

--- ecr-push.sh
+++ ecr-push.sh
@@ -1,6 +1,7 @@
 ECR_URL_BASE=109653375423.dkr.ecr.eu-west-1.amazonaws.com
 SERVICE_NAME=aws-compliance
+REGION=eu-west-1
 REPOSITORY_URI=$ECR_URL_BASE/$SERVICE_NAME
 docker build -t $REPOSITORY_URI:latest .
-aws ecr get-login-password --region eu-west-1 | docker login --username AWS --password-stdin $ECR_URL_BASE
+aws ecr get-login-password --region $REGION | docker login --username AWS --password-stdin $ECR_URL_BASE
 docker push $REPOSITORY_URI:latest
